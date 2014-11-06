78c1753fea1c4e23976b1cda1f70ac4d395c88f9
Steve Calvert <scalvert@mobify.com>
2014-11-06 10:32:51 -0800

    Wrapping tests in extra describe for easier reading

--- tests/unit/formatTests.js
+++ tests/unit/formatTests.js
@@ -2,55 +2,57 @@ define([
     '$',
     'src/format'
 ], function($) {
-    describe('Format with no placeholders', function() {
-        it('returns empty when passed empty', function() {
-            assert.equal($.format(''), '');
-        });
+    describe('Format', function() {
+        describe('with no placeholders', function() {
+            it('returns empty when passed empty', function() {
+                assert.equal($.format(''), '');
+            });
 
-        it('returns empty when passed undefined', function() {
-            assert.equal($.format(), '');
-        });
+            it('returns empty when passed undefined', function() {
+                assert.equal($.format(), '');
+            });
 
-        it('returns empty when passed null or falsy', function() {
-            assert.equal($.format(null), '');
-        });
-    });
-
-    describe('Format with placeholders', function() {
-        it('returns correctly formatted string when single placeholder exists', function() {
-            var formatString = 'Testing replacement with single {0} string';
-            var expectedString = 'Testing replacement with single placeholder string';
-
-            assert.equal($.format(formatString, 'placeholder'), expectedString);
-        });
-
-        it('returns correctly formatted string when multiple placeholders exist', function() {
-            var formatString = 'Testing replacement with multiple {0} {1}';
-            var expectedString = 'Testing replacement with multiple placeholder strings';
-
-            assert.equal($.format(formatString, 'placeholder', 'strings'), expectedString);
-        });
-
-        it('returns correctly formatted string when multiple placeholders exist in variable order', function() {
-            var formatString = 'Testing replacement with multiple {1} {0}';
-            var expectedString = 'Testing replacement with multiple placeholder strings';
-
-            assert.equal($.format(formatString, 'strings', 'placeholder'), expectedString);
-        });
-
-        it('throws an error when placeholders don\'t begin at 0th index', function() {
-            var formatString = 'Testing replacement throws with wrong index {1}';
-
-            assert.throws(function() {
-                $.format(formatString, 'defined');
+            it('returns empty when passed null or falsy', function() {
+                assert.equal($.format(null), '');
             });
         });
 
-        it('throws an error when no replacement strings are passed', function() {
-            var formatString = 'Testing no replacements passed {0}';
+        describe('with placeholders', function() {
+            it('returns correctly formatted string when single placeholder exists', function() {
+                var formatString = 'Testing replacement with single {0} string';
+                var expectedString = 'Testing replacement with single placeholder string';
 
-            assert.throws(function() {
-                $.format(formatString);
+                assert.equal($.format(formatString, 'placeholder'), expectedString);
+            });
+
+            it('returns correctly formatted string when multiple placeholders exist', function() {
+                var formatString = 'Testing replacement with multiple {0} {1}';
+                var expectedString = 'Testing replacement with multiple placeholder strings';
+
+                assert.equal($.format(formatString, 'placeholder', 'strings'), expectedString);
+            });
+
+            it('returns correctly formatted string when multiple placeholders exist in variable order', function() {
+                var formatString = 'Testing replacement with multiple {1} {0}';
+                var expectedString = 'Testing replacement with multiple placeholder strings';
+
+                assert.equal($.format(formatString, 'strings', 'placeholder'), expectedString);
+            });
+
+            it('throws an error when placeholders don\'t begin at 0th index', function() {
+                var formatString = 'Testing replacement throws with wrong index {1}';
+
+                assert.throws(function() {
+                    $.format(formatString, 'defined');
+                });
+            });
+
+            it('throws an error when no replacement strings are passed', function() {
+                var formatString = 'Testing no replacements passed {0}';
+
+                assert.throws(function() {
+                    $.format(formatString);
+                });
             });
         });
     });

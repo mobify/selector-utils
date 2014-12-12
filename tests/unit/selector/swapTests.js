a11d75853078fa0a0fe7425c960012c309f46c2c
Steve Calvert <scalvert@mobify.com>
2014-12-12 09:15:46 -0800

    Updating test to use is to test element type. Added test to ensure attributes and content are converted

--- tests/unit/selector/swapTests.js
+++ tests/unit/selector/swapTests.js
@@ -1,19 +1,37 @@
 define([
     'text!fixtures/swap.html',
+    'text!fixtures/swapWithAttributes.html',
     '$',
     'src/selector/swap'
-], function(fixture, $) {
+], function(fixture, fixtureWithAttributes, $) {
     describe('Swap', function() {
-        describe('attributes with invalid input', function() {
-            it('returns empty when passed empty', function() {
+        describe('when called with empty selector', function() {
+            it('returns empty set', function() {
                 assert.equal($('').swap().length, 0);
             });
         });
 
-        describe('attributes with valid input', function() {
+        describe('when called with selector', function() {
             it('correctly swaps element', function() {
-                assert.equal($(fixture).swap('h2').filter('h1').length, 0);
-                assert.equal($(fixture).swap('h2').filter('h2').length, 1);
+                var $h2 = $(fixture).swap('h2');
+
+                assert.isFalse($h2.is('h1'));
+                assert.isTrue($h2.is('h2'));
+            });
+        });
+
+        describe('when called with selector with attributes', function() {
+            it('correctly swaps element keeping attributes and content', function() {
+                var $span = $(fixtureWithAttributes).swap('span');
+
+                assert.isTrue($span.is('span'));
+
+                assert.isTrue($span.hasClass('some__class'));
+                assert.equal($span.attr('id'), 'someElement');
+                assert.isTrue($span[0].hasAttribute('data-test'));
+                assert.isTrue(!!$span.attr('data-test'));
+                assert.isTrue($span[0].hasAttribute('hidden'));
+                assert.equal($.trim($span.text()), 'Some content in here!');
             });
         });
     });

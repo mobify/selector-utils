d895d39c0a86551c4c1e9c3a0a1850101eb1fe92
Vincent Marta <vmarta@gmail.com>
2015-06-26 13:32:04 -0700

    scrollTo: group together container-related code

--- src/selector/scrollTo.js
+++ src/selector/scrollTo.js
@@ -13,10 +13,20 @@ define([
         options = options || {};
 
         if (options.container) {
-            var $container = $(options.container);
+            var $container = $(options.container).first();
+            var container = $container[0];
+
+            // Ensure that the container is a plain DOM object, as that's what
+            // Velocity requires for non-jQuery environments
+            options.container = container;
+
             if ($container.css('position') === 'static') {
                 $container.css('position', 'relative');
+
                 console.warn('$.scrollTo: the given container must not use position:static, so we made it position:relative');
+                // From Velocity's doc:
+                // "The container element must have its CSS position property 
+                // set to either relative, absolute, or fixed — static will not work"
             }
         }
 
@@ -32,12 +42,6 @@ define([
      */
     $.fn.scrollTo = function(options) {
         return this.each(function() {
-            /**
-             * Ensure that the container is a plain DOM object, as that's what
-             * Velocity requires for non-jQuery environments
-             */
-            options.container && options.container.length && (options.container = $(options.container)[0]);
-
             return $.scrollTo($(this), options);
         });
     };
